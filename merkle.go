3e81455df26f0bd46901e4a56495eb47e5cb60e2
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2023-10-09 17:23:20 +0200

    add Tree.String to visualize its structure while debugging

--- merkle.go
+++ merkle.go
@@ -23,16 +23,12 @@ func FromContentSlice(content [][]byte) Tree {
 		b := make([]byte, 64)
 		sha3.ShakeSum256(b, data)
 
-		fmt.Println("hash:", hashemo.FromBytes(b))
+		//		fmt.Printf("hash: %c\n", []rune(hashemo.FromBytes(b))[0])
 		n := Node{
 			Hash: b,
 		}
 
 		nodes = append(nodes, &n)
-	}
-
-	if len(nodes)%2 != 0 {
-		nodes = append(nodes, nodes[len(nodes)-1])
 	}
 
 	n := buildCousins(nodes)
@@ -44,6 +40,10 @@ func FromContentSlice(content [][]byte) Tree {
 }
 
 func buildCousins(nodes []*Node) *Node {
+	if len(nodes)%2 != 0 {
+		nodes = append(nodes, nodes[len(nodes)-1])
+	}
+
 	var parents []*Node
 	for i := 0; i < len(nodes); i += 2 {
 		left, right := nodes[i], nodes[i+1]
@@ -85,4 +85,42 @@ func (t Tree) Belongs(hash []byte) bool {
 func (t Tree) GenerateProof(hash []byte) [][]byte {
 	// FIXME
 	return nil
+}
+
+func (t Tree) String() string {
+	return buildCousinLine(0, t.Nodes)
+}
+
+func buildCousinLine(depth int, nodes []*Node) string {
+	prefix := ""
+	middle := " "
+	for i := 0; i < depth; i++ {
+		prefix += "  "
+		middle += "  "
+	}
+
+	var s string
+	var parents []*Node
+
+	for i, n := range nodes {
+		if i == 0 {
+			s += fmt.Sprintf("%s", prefix)
+		}
+		s += fmt.Sprintf("%s%s", hashemo.FromBytes(n.Hash[0:1]), middle)
+		if i%2 == 0 {
+			if n.Parent == nil {
+				continue
+			}
+			parents = append(parents, n.Parent)
+		}
+	}
+	s += fmt.Sprintln()
+	if len(parents) == 0 {
+		return s
+	}
+
+	depth++
+	s += buildCousinLine(depth, parents)
+
+	return s
 }
--- merkle_test.go
+++ merkle_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/dolanor/merkle"
-	"golang.org/x/crypto/sha3"
 )
 
 func TestFromContentSlice(t *testing.T) {
@@ -12,11 +11,19 @@ func TestFromContentSlice(t *testing.T) {
 		[]byte("hello"),
 		[]byte("world"),
 		[]byte("yoyo"),
+		[]byte("zozo"),
+		[]byte("magot"),
+		[]byte("hello"),
+		[]byte("world"),
+		[]byte("yoyo"),
+		[]byte("magot"),
 	})
 
 	_ = tree
 
+	t.Logf("\n%s", tree)
 	b := make([]byte, 64)
-	sha3.ShakeSum256(b, []byte("yoyo"))
-	t.Fatal(tree.Belongs(b))
+	// sha3.ShakeSum256(b, []byte("yoyo"))
+	// t.Fatal(tree.Belongs(b))
+	_ = b
 }
